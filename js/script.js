d53281ec829e252448cc4862c1013ec32d7cedc4
Chris Barna <chris@unbrain.net>
2012-05-15 22:10:09 -0700

    New acceleration system.
    It doesn't work super well though.

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@
 
   // Tons of initial conditions.
   var props = {
-    alpha: 0.9,
+    alpha: 0.5,
     beta: 1,
     cr: 3,
-    ca: 10,
-    lr: 20,
-    la: 60,
+    ca: 5,
+    lr: 10,
+    la: 50,
     lc: 4,
     dt: 0.05,
     play: true
@@ -21,8 +21,8 @@
   var gui = new dat.GUI();
   gui.remember(props);
   gui.add(props, "play");
-  gui.add(props, "alpha", 0, 2);
-  gui.add(props, "beta", 0, 2);
+  gui.add(props, "alpha", 0, 5);
+  gui.add(props, "beta", 0, 5);
   gui.add(props, "cr", 0, 25);
   gui.add(props, "ca", 0, 25);
   gui.add(props, "lr", 0, 100);
@@ -44,11 +44,11 @@
     this.x = x;
     this.y = y;
 
-    this.vx = (Math.random() * 5) - 2.5;
-    this.vy = (Math.random() * 5) - 2.5;
+    this.vx = 0; //(Math.random() * 5) - 2.5;
+    this.vy = 0; //(Math.random() * 5) - 2.5;
 
-    this.ax = Math.random() * 0.001;
-    this.ay = Math.random() * 0.001;
+    this.ax = 0; //Math.random() * 0.001;
+    this.ay = 0; //Math.random() * 0.001;
 
     this.element = paper.circle(x, y, 5);
 
@@ -58,7 +58,7 @@
 
       this.x = this.x + this.vx;
       this.y = this.y + this.vy;
-      this.element.animate({cx: this.x, cy: this.y}, 1);
+      this.element.animate({cx: this.x, cy: this.y}, 5);
 
       if (this.x > width || this.x < 0) {
         if (this.x > width) {
@@ -82,14 +82,14 @@
     };
 
     this.adjustVelocity = function () {
-      var x_sum = 0, y_sum = 0, total = 0;
+      var fx = 0, fy = 0;
 
 
       for (var i = 0; i < fish.length; i += 1) {
         var d = Math.sqrt(Math.pow(fish[i].x - this.x, 2)
           + Math.pow(fish[i].y - this.y, 2));
 
-        if (d > 0) {
+        if (d > 0 && d < 2 * props.la) {
           this.ax = this.ax
             + (props.cr * Math.exp(Math.E, -d / props.lr)
                * (-1 / (2 * props.lr))
@@ -97,29 +97,32 @@
             - (props.ca * Math.exp(Math.E, -d / props.la)
                * (-1 / (2 * props.la))
                * (1/d) * (2 * (this.x - fish[i].x)));
+          fx = fx + this.vx * Math.exp(Math.E, -d / props.lc);
 
           this.ay = this.ay
             + (props.cr * Math.exp(Math.E, -d / props.lr)
                * (-1 / (2 * props.lr))
-               * (1/d) * (2 * (this.y - fish[i].y)))
+               * (1) * (2 * (this.y - fish[i].y)))
             - (props.ca * Math.exp(Math.E, -d / props.la)
                * (-1 / (2 * props.la))
-               * (1/d) * (2 * (this.y - fish[i].y)));
+               * (1) * (2 * (this.y - fish[i].y)));
+          fy = fy + this.vy * Math.exp(Math.E, -d / props.lc);
         }
       }
 
-      this.ax = (props.alpha - props.beta)
+      this.ax = fx * props.alpha - props.beta
         * this.vx - this.ax;
 
-      this.ay = (props.alpha - props.beta)
+      this.ay = fy * props.alpha - props.beta
         * this.vy - this.ay;
+
 
     };
   };
 
   window.fish = [];
 
-  for (var i = 0; i < 50; i += 1) {
+  for (var i = 0; i < 25; i += 1) {
     var randx = Math.random() * width;
     var randy = Math.random() * height;
     fish.push(new Fish(randx, randy));
@@ -131,12 +134,12 @@
       for (var i = 0; i < fish.length; i += 1) {
         fish[i].motion();
         if (i === 1) {
-          console.log(fish[i].ax);
+          console.log(fish[i].vy);
         }
       }
     }
   }
 
-  var animationInterval = setInterval(animate, 50);
+  var animationInterval = setInterval(animate, 100);
 
 })();
